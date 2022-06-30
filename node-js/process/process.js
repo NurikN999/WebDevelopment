deae3779e9b56ef52a08a445c4546300dc0fcd80
NurikN999 <nurkamalnurmukhamed@gmail.com>
2022-07-01 00:25:32 +0600

    process_nodejs

---
+++ node-js/process/process.js
@@ -0,0 +1,8 @@
+const process = require('process');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+console.log(process.pid);
+console.log(process.env.PORT);
+console.log(process.env.NODE_ENV)
