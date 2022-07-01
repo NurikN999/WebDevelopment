236260b115cce341f50650f0dc774e6b02adadc5
NurikN999 <nurkamalnurmukhamed@gmail.com>
2022-07-01 20:16:33 +0600

    url_path_lesson

--- node-js/lessons/path.js
+++ node-js/lessons/path.js
@@ -8,3 +8,12 @@ console.log(path.relative('/first', 'second', 'third'));
 
 //Get dirname (Absolute Path)
 console.log(__dirname);
+
+//Parse path
+console.log(path.parse(__dirname));
+
+
+//URL
+const siteUrl = 'http://localhost:8080/users?id=15'
+const url = new URL(siteUrl)
+console.log(url)
